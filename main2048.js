1b43f5a3d12596032b7fafc162ad01d6b6cc405c
llinzeyang <linzeyangcd@foxmail.com>
2017-03-24 13:12:23 +0800

    Update main2048.js

--- main2048.js
+++ main2048.js
@@ -6,6 +6,7 @@ var startX,startY,endX,endY;
 $(document).ready(function(){
 	prepareForMobile();//适应移动端尺寸
 	newgame();
+	stopDrop();
 })
 
 function prepareForMobile(){
@@ -162,6 +163,23 @@ document.addEventListener('touchstart',function(event){
   startX=event.touches[0].pageX;
   startY=event.touches[0].pageY;
 });
+
+function stopDrop() {
+    var lastY;//最后一次y坐标点
+    $(document.body).on('touchstart', function(event) {
+        lastY = event.originalEvent.changedTouches[0].clientY;//点击屏幕时记录最后一次Y度坐标。
+    });
+    $(document.body).on('touchmove', function(event) {
+        var y = event.originalEvent.changedTouches[0].clientY;
+        var st = $(this).scrollTop(); //滚动条高度  
+        if (y >= lastY && st <= 10) {//如果滚动条高度小于0，可以理解为到顶了，且是下拉情况下，阻止touchmove事件。
+            lastY = y;
+            event.preventDefault();
+        }
+        lastY = y;
+ 
+    });
+}
 
 document.addEventListener('touchmove',function(event){
   event.preventDefault();
